60a2cdc14d0e072e8ada21998525b0e1c1a1d70b
dobneves <dobneves@mppr.mp.br>
2020-11-27 20:34:23 -0300

    Removed temporary javascript file

--- app.js
+++
@@ -1,8 +0,0 @@
-const name = document.getElementById("aqui");
-
-console.log(document.querySelectorAll("daniel path"))
-console.log(document.getElementsByTagName("path"))
-console.log(name);
-for (let i = 0; i < name.length; i++) {
-  console.log(`letter ${i} is ${name[i].getTotalLength()}`)
-}
